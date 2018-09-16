321c643557711e0cb04e4fb4e2c7ebb26f8fe5e4
MoMuilWH <43278110+MoMuilWH@users.noreply.github.com>
2018-09-16 14:50:09 +0300

    metasploit

---
+++ meta.sh
@@ -0,0 +1,8 @@
+apt install git python python2 curl wget nano figlet -y
+
+figlet MoMuilWH
+
+git clone https://github.com/rapid7/metasploit-framework
+cd metasploit-framework
+bundle update nokogiri
+
